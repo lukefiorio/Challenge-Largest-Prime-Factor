e0906fd34a750d21786216a5c2b5a2f9e13c479b
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-03 18:53:18 -1000

    completed assignment

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -8,32 +8,5 @@ exports.largestPrimeFactor = function (n) {
       primeNumber++;
     }
   }
-
   return primeNumber;
 };
-
-/* lukes best version
-function largestPrime4(n) {
-  let primeNumber = 0;
-  // do your work here
-  let isPrime = [2];
-
-  for (let i = 3; i <= Math.floor(n / 2); i++) {
-    let newPrime = 1;
-    for (let j = 0; (j < isPrime.length) && (newPrime === 1); j++) {
-      if (i % isPrime[j] === 0) newPrime = 0;
-    }
-    if (newPrime === 1) isPrime.push(i);
-  }
-
-  for (let k = isPrime.length - 1; (k >= 0) && (primeNumber === 0); k--) {
-    if (n % isPrime[k] === 0) primeNumber = isPrime[k];
-  }
-
-  return primeNumber;
-}
-*/
-
-//console.time("V4");
-//console.log(largestPrime4(734873));
-//console.timeEnd("V4");
